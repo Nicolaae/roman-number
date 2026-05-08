9d3713eb9b8be4d45276c010b0f8356bd5516581
Nicolaae <gava.nicola05@gmail.com>
2026-05-08 14:46:06 +0200

    Setup iniziale progetto Maven, Closes #1

---
+++ src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -0,0 +1,14 @@
+////////////////////////////////////////////////////////////////////
+// Nicola Gava 2148613
+// Julvi Myrtaj 2148612
+////////////////////////////////////////////////////////////////////
+
+package it.unipd.mtss;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class RomanPrinterTest {
+
+    }
+}
